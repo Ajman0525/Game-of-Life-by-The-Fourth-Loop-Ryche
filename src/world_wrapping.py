446b66e86d9eec50e65aed74a6f3208b4287d72d
Ajman <ajman.mocsana@g.msuiit.edu.ph>
2025-11-02 20:43:58 +0800

    refactor: make the test two dimensional

--- src/world_wrapping.py
+++ src/world_wrapping.py
@@ -1,11 +1,11 @@
 class Grid_Wrapper:
-    def __init__(self,size):
-        self.dimension_size = size
+    def __init__(self,width, height):
+        self.grid_width = width
+        self.grid_height = height
 
-    def world_wrap(self, current_coordinate,change_in_coordinate):
+    def world_wrap(self, current_coordinate,change_in_coordinate, total_cells):
         new_index_coordinate = current_coordinate + change_in_coordinate
-        size = self.dimension_size
-        wrapped_coordinate = (new_index_coordinate % size + size) % size
+        wrapped_coordinate = (new_index_coordinate % total_cells + total_cells) % total_cells
 
         return wrapped_coordinate
 
--- test/test_toroidal_grid.py
+++ test/test_toroidal_grid.py
@@ -1,9 +1,9 @@
 from src.world_wrapping import Grid_Wrapper
 def test_world_wrapping_from_start_to_end():
     DIMENSION_SIZE = 10
-    connect = Grid_Wrapper(size=DIMENSION_SIZE)
+    connect = Grid_Wrapper(width=DIMENSION_SIZE, height=DIMENSION_SIZE)
 
     current_coordinate = 0
     change_in_coordinate = -1
 
-    assert connect.world_wrap(current_coordinate, change_in_coordinate) == 9
+    assert connect.world_wrap(current_coordinate, change_in_coordinate, DIMENSION_SIZE) == 9
