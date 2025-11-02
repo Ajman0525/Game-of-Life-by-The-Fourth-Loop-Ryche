d55620035dbdf09f7eb9eae7d6fd8714a3366887
Ajman <ajman.mocsana@g.msuiit.edu.ph>
2025-11-02 20:17:49 +0800

    refactor: test the grid wrapping logic

--- src/world_wrapping.py
+++ src/world_wrapping.py
@@ -1,3 +1,11 @@
 class Grid_Wrapper:
-    def world(self):
-        return True
+    def __init__(self,size):
+        self.dimension_size = size
+
+    def world_wrap(self, current_coordinate,change_in_coordinate):
+        new_index_coordinate = current_coordinate + change_in_coordinate
+        size = self.dimension_size
+        wrapped_coordinate = (new_index_coordinate % size + size) % size
+
+        return wrapped_coordinate
+
--- test/test_toroidal_grid.py
+++ test/test_toroidal_grid.py
@@ -1,5 +1,9 @@
 from src.world_wrapping import Grid_Wrapper
-def test_world_wrapping():
-    connect = Grid_Wrapper()
+def test_world_wrapping_from_start_to_end():
+    DIMENSION_SIZE = 10
+    connect = Grid_Wrapper(size=DIMENSION_SIZE)
 
-    assert connect.world() is True
+    current_coordinate = 0
+    change_in_coordinate = -1
+
+    assert connect.world_wrap(current_coordinate, change_in_coordinate) == 9
